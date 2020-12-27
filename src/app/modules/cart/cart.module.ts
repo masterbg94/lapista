79ad09e9c7b4c8ca7fd9b42d4091f78601e9b90f
Nemanja Milinkovic <nmilinkovic46@gmail.com>
2020-12-27 20:31:45 +0100

    [cart updates] * cart number * delete local storage

--- src/app/modules/cart/cart.module.ts
+++ src/app/modules/cart/cart.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CartRoutingModule} from './cart.routing.module';
 import {CartComponent} from './pages/cart.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [CartComponent],
-  imports: [CartRoutingModule]
+  imports: [CartRoutingModule, CommonModule]
 })
 export class CartModule {
 }
--- src/app/modules/cart/pages/cart.component.ts
+++ src/app/modules/cart/pages/cart.component.ts
@@ -1,8 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  shoppingItems;
+
+  ngOnInit(): void {
+    this.getItemsFromLocalStorage();
+  }
+
+  getItemsFromLocalStorage() {
+    this.shoppingItems = JSON.parse(localStorage.getItem('cart'));
+    console.log('shoppingItems', this.shoppingItems);
+  }
+
+  deleteLocalStorage() {
+    localStorage.removeItem('cart');
+    // TODO: ispraviti bug kada se sve obrise ne menja se cart number()
+    // this.getItemsFromLocalStorage();
+    window.location.reload();
+  }
 }

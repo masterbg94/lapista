990ca9f2b030c2df0f0d77c4a001ad266172d244
Nemanja Milinkovic <nmilinkovic46@gmail.com>
2021-02-03 01:07:27 +0100

    [Contact map] * added map to contact * fixed ""@types/googlemaps": "3.39.12" to prevent errors in build

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -1,11 +1,17 @@
-import {NgModule} from '@angular/core';
-import {ContactComponent} from './pages/contact.component';
-import {ContactRoutingModule} from './contact.routing.module';
-import {ContactFormModule} from '../../core/components/contact-form/contact-form.module';
+import { NgModule } from '@angular/core';
+import { ContactComponent } from './pages/contact.component';
+import { ContactRoutingModule } from './contact.routing.module';
+import { ContactFormModule } from '../../core/components/contact-form/contact-form.module';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
-  imports: [ContactRoutingModule, ContactFormModule],
-  declarations: [ContactComponent]
+  imports: [
+    ContactRoutingModule,
+    ContactFormModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDf0Z-oUxKPN65T17S_6eLWwyVc74UW4-A',
+    }),
+  ],
+  declarations: [ContactComponent],
 })
-export class ContactModule {
-}
+export class ContactModule {}
--- src/app/modules/contact/pages/contact.component.ts
+++ src/app/modules/contact/pages/contact.component.ts
@@ -5,4 +5,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  title = 'My first AGM project';
+  lat = 44.809038;
+  lng = 20.403220;
 }

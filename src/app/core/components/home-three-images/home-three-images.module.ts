952f400fd76b61d2d32e086ae3f175fa3147c64e
Nemanja Milinkovic <nmilinkovic46@gmail.com>
2021-02-27 16:04:31 +0100

    [mobile toolbar fix,added lang,contact page fix] * added language select option for mobile devices * fixed style and typo in contact * fixed contact map scroll on desktop

--- src/app/core/components/home-three-images/home-three-images.module.ts
+++ src/app/core/components/home-three-images/home-three-images.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { HomeThreeImagesComponent } from './home-three-images.component';
 import {RouterModule} from '@angular/router';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
-  imports: [
-    RouterModule
-  ],
+    imports: [
+        RouterModule,
+        TranslateModule
+    ],
   exports: [HomeThreeImagesComponent],
   declarations: [HomeThreeImagesComponent],
 })

173e9c81d8e2ac8959077201c90b1691dfe8b075
masterbg94 <nmilinkovic3@gmail.com>
2021-10-26 14:40:23 +0200

    -Fix

--- src/app/layouts/main/main-layout.component.ts
+++ src/app/layouts/main/main-layout.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-main-layout-component',
-  template: '<app-toolbar></app-toolbar><div class="main" style="padding-top: 40px"><router-outlet ></router-outlet></div><app-footer></app-footer>'
+    selector: 'app-main-layout-component',
+    template: '<app-toolbar></app-toolbar><div class="main" style="padding-top: 40px"><router-outlet ></router-outlet></div><app-footer></app-footer>'
 })
 export class MainLayoutComponent {
 }

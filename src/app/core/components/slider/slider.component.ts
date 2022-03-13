07d2161891087bd63e03e5fea887f89f49c1655f
masterbg94 <nmilinkovic3@gmail.com>
2022-03-13 22:15:38 +0100

    -Slider fix

--- src/app/core/components/slider/slider.component.ts
+++ src/app/core/components/slider/slider.component.ts
@@ -1,51 +1,54 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
 import {FootwearModel} from '../../models/footwear-model';
 import {SliderCarouselModel} from '../../models/slider-carousel.model';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
     selector: 'app-slider',
     templateUrl: './slider.component.html',
     styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent implements OnInit, OnChanges {
+export class SliderComponent implements OnInit {
     @Input() slideItems: FootwearModel[] | Array<string>;
     @Input() slideType = 'product';
+    isHandset: boolean;
+
+    constructor(private breakpointObserver: BreakpointObserver) {
+    }
 
     sliderSettings: SliderCarouselModel =
         {
-        height: 300,
-        width: 100,
-        cellWidth: '100%',
-        cellsToShow: 4,
-        // cellsToScroll: number,
-        loop: true,
-        // overflowCellsLimit: number,
-        autoplay: true,
-        freeScroll: false,
-        autoplayInterval: 3000,
-        pauseOnHover: true,
-        dots: true,
-        objectFit: 'contain',
-        margin: 10,
-        transitionDuration: 500,
-        transitionTimingFunction: 'ease-in-out',
-        // counter: false,
-        // counterSeparator: string,
-        borderRadius: 15,
-        arrows: true,
-        arrowsOutside: true,
-        arrowsTheme: 'dark'
-    };
-
-    constructor() {
-    }
+            height: 300,
+            width: 100,
+            cellWidth: '100%',
+            cellsToShow: this.isHandset ? 2 : 4,
+            // cellsToScroll: number,
+            loop: true,
+            // overflowCellsLimit: number,
+            autoplay: true,
+            freeScroll: false,
+            autoplayInterval: 3000,
+            pauseOnHover: true,
+            dots: true,
+            objectFit: 'contain',
+            margin: 10,
+            transitionDuration: 500,
+            transitionTimingFunction: 'ease-in-out',
+            // counter: false,
+            // counterSeparator: string,
+            borderRadius: 15,
+            arrows: true,
+            arrowsOutside: false,
+            arrowsTheme: 'dark'
+        };
 
     ngOnInit(): void {
-        console.log('this.slideItems', this.slideItems);
-    }
-
-    ngOnChanges(changes: SimpleChanges): void {
-        console.log('this.slideItems', this.slideItems);
+        this.breakpointObserver.observe(Breakpoints.Handset).subscribe(result => {
+            console.log('result', result);
+            if (result.matches) {
+                this.isHandset = result.matches;
+            }
+        });
     }
 
 }
